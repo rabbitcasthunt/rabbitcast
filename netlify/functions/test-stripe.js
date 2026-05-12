390848f22beb2b9c1069c613c890cd94dee3642d
rabbitcasthunt <rabbitcasthunt@gmail.com>
2026-05-12 12:00:13 -0400

    Update test-stripe.js

--- netlify/functions/test-stripe.js
+++ netlify/functions/test-stripe.js
@@ -1,1 +1,12 @@
-
+exports.handler = async (event) => {
+  const secretKey = process.env.STRIPE_SECRET_KEY;
+  return {
+    statusCode: 200,
+    headers: { "Access-Control-Allow-Origin": "*" },
+    body: JSON.stringify({
+      hasKey: !!secretKey,
+      keyPrefix: secretKey ? secretKey.substring(0, 7) : "NOT FOUND",
+      nodeVersion: process.version,
+    }),
+  };
+};
